2dc3ce9ec0ece8136f6b2a1f1287aa9afdb38d00
marikgryc <markogricuk@gmail.com>
2026-03-04 18:56:09 +0200

    btn 1 2 3

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.calc
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvDisplay: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvDisplay = findViewById(R.id.tvDisplay)
+
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn3 = findViewById<Button>(R.id.btn3)
+
+        btn1.setOnClickListener { addDigit("1") }
+        btn2.setOnClickListener { addDigit("2") }
+        btn3.setOnClickListener { addDigit("3") }
+    }
+
+    private fun addDigit(digit: String) {
+        if (tvDisplay.text == "0") {
+            tvDisplay.text = digit
+        } else {
+            tvDisplay.append(digit)
         }
     }
 }
